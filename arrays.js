b806e63a6d73830ca369fe6b66eac08fa79bbdc5
Reid Boudreau <rtboudreau@gmail.com>
2018-12-16 22:07:44 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -27,10 +27,14 @@ function accessElementInArray(chocolateBars){
 function destructivelyRemoveElementFromBeginningOfArray(chocolateBars){
   chocolateBars.shift();
   return chocolateBars;
-  
 }
 
 function removeElementFromBeginningOfArray(chocolateBars){
   chocolateBars = chocolateBars.slice(1)
+  return chocolateBars;
+}
+
+function destructivelyRemoveElementFromEndOfArray(chocolateBars){
+  chocolateBars.pop(-1);
   return chocolateBars;
 }
